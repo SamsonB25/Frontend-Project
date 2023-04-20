bcce6ac8fe8b81918d280764b8ff8c2d556e5b6b
SamsonB25 <SamsonB25@yahoo.com>
2023-04-20 00:47:47 -0500

    Able to search by full name

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
+// i need to figure out has to search for a player by name instead of index
+// might have to use a for and for in loop to get access
+async function getName(input) {
+  const response = await fetch(
+    "https://api.sportsdata.io/v3/nfl/scores/json/Players/buf?key=436a1b238845470ea2a5946e906348e8"
+  );
+  const data = await response.json();
+  let name = data.find((obj) => obj.Name === input).Name;
+  let index = data.findIndex((obj) => obj.Name === input);
+  console.log(name + index);
+}
+getName("Josh Allen");
+
 let playerData;
+
 const playerContainer = document.getElementById("player-container");
+
 // create async function to recieve data from api and ba able to call it later.
 async function getPlayerData(player) {
   // create a try...catch statement
@@ -8,15 +23,22 @@ async function getPlayerData(player) {
       "https://api.sportsdata.io/v3/nfl/scores/json/Players/buf?key=436a1b238845470ea2a5946e906348e8"
     );
     const data = await response.json();
+    // console.log(data);
+
+    let name = data.find((obj) => obj.Name === player).Name;
+
+    let index = data.findIndex((obj) => obj.Name === player);
+
     playerData = {
-      fullName: data[player].Name,
-      jerseyNum: data[player].Number,
-      position: data[player].Position,
-      height: data[player].Height,
-      weight: data[player].Weight,
-      status: data[player].Status,
-      img: data[player].PhotoUrl,
+      fullName: data[index].Name,
+      jerseyNum: data[index].Number,
+      position: data[index].Position,
+      height: data[index].Height,
+      weight: data[index].Weight,
+      status: data[index].Status,
+      img: data[index].PhotoUrl,
     };
+
     let html = `<div id="testing" class="card" style="width: 13rem">
       <img
         src="${playerData.img}"
@@ -31,13 +53,8 @@ async function getPlayerData(player) {
       </div>
     </div>`;
     playerContainer.insertAdjacentHTML("afterbegin", html);
-    console.log(data);
-    // console.log(playerData.fullName);
-    // console.log(playerData.jerseyNum);
-    // console.log(playerData.position);
-    // console.log(playerData.height);
-    // console.log(playerData.weight);
-    // console.log(playerData.status);
+
+    console.log(data[player]);
   } catch (error) {
     console.error(error);
   }
@@ -45,16 +62,14 @@ async function getPlayerData(player) {
 
 // create event listener to get user input of the search bar
 const searchBtn = document.getElementById("search_button");
-const clearBtn = document.getElementById("clear_button");
-const searchBar = document.getElementById("search_bar");
+var searchBar = document.getElementById("search_bar");
 
 searchBtn.addEventListener("click", function () {
-  const searchValue = searchBar.value;
+  let searchValue = searchBar.value;
+
   let searchedPlayer = getPlayerData(searchValue);
 
   console.log(searchedPlayer);
-  searchBar.value = "";
 
-  const testing = document.getElementById("testing");
-  testing.remove();
+  searchBar.value = "";
 });
